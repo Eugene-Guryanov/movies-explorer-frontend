28759119451a01c96960d485d12e302fd4c2b8cb
Eugene-Guryanov <103857729+Eugene-Guryanov@users.noreply.github.com>
2023-06-14 16:01:55 +0300

    изменяет регулярные выражения

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,6 +2,4 @@ export const MAIN_API_BASE_URL = 'https://ges.nomoredomains.monster/';
 
 export const MOVIES_API_BASE_URL = 'https://api.nomoreparties.co/beatfilm-movies/';
 
-export const NameRegExp = /[A-Za-zА-Яа-яЁё-]+[A-Za-zА-Яа-яЁё\\s-]*[A-Za-zА-Яа-яЁё-]*/;
-export const EmailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-
+export const EmailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
